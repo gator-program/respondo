6afcd737d44140e39b89c613b1509abf310306d3
Maximilian Scheurer <maximilian.scheurer@iwr.uni-heidelberg.de>
2022-11-13 22:47:26 +0100

    reviews and formatting

--- respondo/jacobi_diis.py
+++ respondo/jacobi_diis.py
@@ -10,12 +10,12 @@ from adcc.solver.explicit_symmetrisation import IndexSymmetrisation
 
 class State:
     def __init__(self):
-        self.solution = None       # Current approximation to the solution
-        self.residual = None       # Current residual
+        self.solution = None  # Current approximation to the solution
+        self.residual = None  # Current residual
         self.residual_norm = None  # Current residual norm
-        self.converged = False     # Flag whether iteration is converged
-        self.n_iter = 0            # Number of iterations
-        self.n_applies = 0         # Number of applies
+        self.converged = False  # Flag whether iteration is converged
+        self.n_iter = 0  # Number of iterations
+        self.n_applies = 0  # Number of applies
 
 
 def default_print(state, identifier, file=sys.stdout):
@@ -23,16 +23,24 @@ def default_print(state, identifier, file=sys.stdout):
         print("Niter residual_norm", file=file)
     elif identifier == "next_iter":
         fmt = "{n_iter:3d}  {residual:12.5g}"
-        print(fmt.format(n_iter=state.n_iter,
-                         residual=np.max(state.residual_norm)), file=file)
+        print(fmt.format(n_iter=state.n_iter, residual=np.max(state.residual_norm)), file=file)
     elif identifier == "is_converged":
         print("=== Converged ===", file=file)
         print("    Number of matrix applies:   ", state.n_applies)
 
 
-def jacobi_diis(matrix, rhs, x0, Dinv, conv_tol=1e-9, max_iter=100,
-                callback=None, explicit_symmetrisation=IndexSymmetrisation,
-                max_subspace=8):
+def jacobi_diis(
+    matrix,
+    rhs,
+    x0,
+    Dinv,
+    conv_tol=1e-9,
+    max_iter=100,
+    callback=None,
+    explicit_symmetrisation=IndexSymmetrisation,
+    diis_max_subspace=10,
+    diis_start_iter=5,
+):
     """An implementation of the Jacobi-DIIS algorithm.
     It solves `matrix @ x = rhs` for `x` by minimising the residual
     `matrix @ x - rhs` using a Jacobi update with DIIS acceleration.
@@ -51,18 +59,22 @@ def jacobi_diis(matrix, rhs, x0, Dinv, conv_tol=1e-9, max_iter=100,
         them converged.
     max_iter : int
         Maximum number of iterations
-    callback
+    callback : callable
         Callback to call after each iteration
     explicit_symmetrisation
         Explicit symmetrisation to perform during iteration to ensure
         obtaining an eigenvector with matching symmetry criteria.
+    diis_max_subspace : int
+        Maximum number of error vectors to keep in DIIS.
+    diis_start_iter: int
+        Number of iterations at which to start DIIS mixing.
     """
     if callback is None:
+
         def callback(state, identifier):
             pass
 
-    if explicit_symmetrisation is not None and \
-            isinstance(explicit_symmetrisation, type):
+    if explicit_symmetrisation is not None and isinstance(explicit_symmetrisation, type):
         explicit_symmetrisation = explicit_symmetrisation(matrix)
 
     if x0 is None:
@@ -83,10 +95,11 @@ def jacobi_diis(matrix, rhs, x0, Dinv, conv_tol=1e-9, max_iter=100,
     if explicit_symmetrisation:
         state.solution = explicit_symmetrisation.symmetrise(state.solution)
     state.residual = evaluate(rhs - matrix @ state.solution)
+    state.n_applies += 1
     state.residual_norm = np.sqrt(state.residual @ state.residual)
 
     callback(state, "start")
-    diis = DIIS(max_subspace=max_subspace)
+    diis = DIIS(max_subspace=diis_max_subspace, start_iter=diis_start_iter)
     while state.n_iter < max_iter:
         state.n_iter += 1
 
@@ -97,14 +110,16 @@ def jacobi_diis(matrix, rhs, x0, Dinv, conv_tol=1e-9, max_iter=100,
             return state
 
         if state.n_iter == max_iter:
-            raise la.LinAlgError("Maximum number of iterations (== "
-                                 + str(max_iter) + " reached in Jacobi/DIIS "
-                                 "procedure.")
+            raise la.LinAlgError(
+                "Maximum number of iterations (== " + str(max_iter) + " reached in Jacobi/DIIS "
+                "procedure."
+            )
 
-        x_new = evaluate(state.solution + Dinv @ state.residual)
+        jacobi_step = evaluate(Dinv @ state.residual)
         if explicit_symmetrisation:
-            x_new = explicit_symmetrisation.symmetrise(x_new)
-        state.solution = diis.extrapolate(x_new, x_new - state.solution)
+            jacobi_step = explicit_symmetrisation.symmetrise(jacobi_step)
+        x_new = evaluate(state.solution + jacobi_step)
+        state.solution = diis.extrapolate(x_new, jacobi_step)
 
         state.residual = evaluate(rhs - matrix @ state.solution)
         state.n_applies += 1
@@ -112,9 +127,9 @@ def jacobi_diis(matrix, rhs, x0, Dinv, conv_tol=1e-9, max_iter=100,
 
 
 class DIIS:
-    # adapted from 
+    # adapted from
     # https://github.com/edeprince3/pdaggerq/blob/master/examples/full_cc_codes/diis.py
-    def __init__(self, max_subspace, start_iter=3):
+    def __init__(self, max_subspace=10, start_iter=5):
         self.nvecs = max_subspace
         self.error_vecs = []
         self.prev_vecs = []
