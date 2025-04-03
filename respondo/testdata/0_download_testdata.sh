c492e1c45278582c9d8390b4eb47a1b4f14e7d1d
Friederike Schneider <friederike.schneider@iwr.uni-heidelberg.de>
2025-04-03 15:51:52 +0200

    store testdata in repo as temporary fix

---
+++ respondo/testdata/0_download_testdata.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# taken from adcc repository, written by @mfherbst
+
+# SOURCE="https://wwwagdreuw.iwr.uni-heidelberg.de/respondo_test_data/0.3.0"
+DATAFILES=(
+	data_0.3.0.tar.gz
+)
+#
+# -----
+#
+
+THISDIR=$(dirname "${BASH_SOURCE[0]}")
+cd "$THISDIR"
+echo "Updating testdata ... please wait."
+
+# 
+# ToDo: temporary fix store testdata in repository.
+# As soon as new server is online move testdata there
+# download() {
+# 	if which wget &> /dev/null; then
+# 	    echo $@
+# 		wget -w 1 -qN --show-progress --no-check-certificate $@
+# 	else
+# 		echo "wget not installed" >&2
+# 		exit 1
+# 	fi
+# }
+
+# download $(for file in ${DATAFILES[@]}; do echo $SOURCE/$file; done)
+
+if which sha256sum &> /dev/null; then
+	sha256sum --ignore-missing -c SHA256SUMS || exit 1
+fi
+
+for file in ${DATAFILES[@]}; do
+    tar -xzf $file
+done
+
+exit 0
